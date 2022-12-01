2b1ec09673471a93f5d6086f57ae92bde1dfc20d
Vlad <vladyslav.o@enkonix.com>
2022-12-01 23:26:59 +0200

    WOrk

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,9 @@
 <template>
-  <p>Hello, world#</p>
+  <div class="container mx-auto py-8">
+    <div class="text-center">
+      <NuxtLink to="course" class="bg-blue-500 p-4 rounded-md">
+        Start Learn
+      </NuxtLink>
+    </div>
+  </div>
 </template>
