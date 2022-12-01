002a6d704cc9ef83f2a385c3101c222295689b5e
Vlad <vladyslav.o@enkonix.com>
2022-12-01 17:37:07 +0200

    Work

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -3,4 +3,24 @@
   <p>This is a lesson</p>
   <p>{{ $route.params.chapterSlug }}</p>
   <p>{{ $route.params.lessonSlug }}</p>
+  <hr />
+  <p>{{ chapter }}</p>
+  <p>{{ lesson }}</p>
 </template>
+
+<script setup>
+const course = useCourse();
+const route = useRoute();
+
+const chapter = computed(() => {
+  return course.chapters.find(
+    (chapter) => chapter.slug === route.params.chapterSlug
+  );
+});
+
+const lesson = computed(() => {
+  return chapter.value.lessons.find(
+    (lesson) => lesson.slug === route.params.lessonSlug
+  );
+});
+</script>
